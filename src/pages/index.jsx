7169f03e3e977cdc788004d7dad06d217ad01eec
NV21061-Anas-Khalid <118424723+NV21061-Anas-Khalid@users.noreply.github.com>
2022-12-26 11:13:51 +0300

    ooo1

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -257,7 +257,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            CCP Student, stades cloud.
+            CCP Student, studys cloud.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Anas, a software Engener student from Bahrain. Me and my group we are the first under 18 students in bahrain stading Cloud computing
