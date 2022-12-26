8d72fd5b75462ec86d7fb6089ca8fcb7c23627b8
NV21061-Anas-Khalid <118424723+NV21061-Anas-Khalid@users.noreply.github.com>
2022-12-26 11:39:27 +0300

    lol1

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -11,28 +11,28 @@ import logoPlanetaria from '@/images/logos/planetaria.svg'
 
 const projects = [
   {
-    name: 'Planetaria',
+    name: 'DataBase project',
     description:
       'Creating technology to empower civilians to explore space on their own terms.',
     link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
     logo: logoPlanetaria,
   },
   {
-    name: 'Animaginary',
+    name: 'Internet Programing',
     description:
       'High performance web animation library, hand-written in optimized WASM.',
     link: { href: '#', label: 'github.com' },
     logo: logoAnimaginary,
   },
   {
-    name: 'HelioStream',
+    name: 'Enjase project',
     description:
-      'Real-time video streaming library, optimized for interstellar transmission.',
+      'We did a telescopic laptop table.',
     link: { href: '#', label: 'github.com' },
     logo: logoHelioStream,
   },
   {
-    name: 'cosmOS',
+    name: 'linux project',
     description:
       'The operating system that powers our Planetaria space shuttles.',
     link: { href: '#', label: 'github.com' },
